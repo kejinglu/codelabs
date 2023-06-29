70829008730a3c1b3f75bca9af819091445404a9
kejinglu <nydove@me.com>
2023-06-28 23:15:13 -0600

    pt5 activatedroute

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-details',
@@ -8,4 +9,10 @@ import { CommonModule } from '@angular/common';
   templateUrl: './details.component.html',
   styleUrls: ['./details.component.css']
 })
-export class DetailsComponent {} //compomemt name is DetailsComponent
+export class DetailsComponent { //compomemt name is DetailsComponent
+  route: ActivatedRoute = inject(ActivatedRoute);
+  housingLocationId = 0;
+  constructor(){
+    this.housingLocationId = Number(this.route.snapshot.params['id']);
+  }
+} 
